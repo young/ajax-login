501bd50534344786eea6102fd80d8c535c195064
Jem Young <jemuel.young@gmail.com>
2013-03-19 21:52:10 -0400

    Change file names. Move base.view to own file

--- javascript/views/base.view.js
+++ javascript/views/base.view.js
@@ -1,6 +1,3 @@
-// javascript/main.js
-
-
 // application base class
 // place helper functions in here
 window.Application = {
@@ -31,31 +28,3 @@ window.Application = {
 		return true;
 	}
 };
-
-Application.Router = Backbone.Router.extend({
-
-	routers : {
-		"login" : "login"
-	},
-	
-	// setup main page to load when application starts
-	initialize: function(){
-		var self = this;
-		// render the login view
-		self.LoginView = $("#main").html(new Application.LoginView().render().el);
-	},
-	
-	// login view
-	login : function() {
-		var self = this;
-		if(self.Loginview){
-			self.LoginView.login("#");
-		} else {
-			self.LoginView = $("#main").html(new Application.LoginView().render().el);
-			self.LoginView.login("#");
-		}
-
-	}
-});
-
-
