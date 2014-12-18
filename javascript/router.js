332eab0512995a6c48a305d3b60dc7e6c898c59e
Jem Young <jemuel.young@gmail.com>
2014-12-18 00:58:35 -0500

    Minor updates

--- javascript/router.js
+++ javascript/router.js
@@ -2,28 +2,28 @@
 
 Application.Router = Backbone.Router.extend({
 
-	routers : {
-		"login" : "login"
-	},
-	
-	// setup main page to load when application starts
-	initialize: function(){
-		var self = this;
-		// render the login view
-		self.LoginView = $("#main").html(new Application.LoginView().render().el);
-	},
-	
-	// login view
-	login : function() {
-		var self = this;
-		if(self.Loginview){
-			self.LoginView.login("#");
-		} else {
-			self.LoginView = $("#main").html(new Application.LoginView().render().el);
-			self.LoginView.login("#");
-		}
+  routers : {
+    "login" : "login"
+  },
+  
+  // setup main page to load when application starts
+  initialize: function(){
+    var self = this;
+    // render the login view
+    self.LoginView = $("#main").html(new Application.LoginView().render().el);
+  },
+  
+  // login view
+  login : function() {
+    var self = this;
+    if(self.Loginview){
+      self.LoginView.login("#");
+    } else {
+      self.LoginView = $("#main").html(new Application.LoginView().render().el);
+      self.LoginView.login("#");
+    }
 
-	}
+  }
 });
 
 
--- javascript/views/base.view.js
+++ javascript/views/base.view.js
@@ -1,30 +1,32 @@
-// application base class
-// place helper functions in here
+// Application base class
+
+// Place helper functions in here
 window.Application = {
-	// synchronously load a template from templates directory
-	getTemplate : function(name) {
-		var self = this, url;
-		self.assert(_.isUndefined(name) || name == "", "Template name is required"); // check for template name
-		template = "<h1 class='red'>Failed to load template" + name + "</h1>";
-		url = "templates/" + name;
-		$.ajax({
-			async : false,
-			url : url,
-			success : function(response) {
-				template = response;
-			}
-		});
-		return template;
-	},
-	
-	// assert
-	assert : function(test, message) {
-		var self = this;
-		
-		if(test){
-			console.log(message);
-			return false;
-		}
-		return true;
-	}
+  // synchronously load a template from templates directory
+  getTemplate : function(name) {
+    var self = this, url;
+    // Check for template name
+    self.assert(_.isUndefined(name) || name === "", "Template name is required");
+    template = "<h1 class='red'>Failed to load template" + name + "</h1>";
+    url = "templates/" + name;
+    $.ajax({
+      async : false,
+      url : url,
+      success : function(response) {
+        template = response;
+      }
+    });
+    return template;
+  },
+  
+  // assert
+  assert : function(test, message) {
+    var self = this;
+    
+    if(test){
+      console.log(message);
+      return false;
+    }
+    return true;
+  }
 };
